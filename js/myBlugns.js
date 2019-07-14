9fb7e3ca567498528a7db6119c0dc718d863a6fb
khadada <lkhadada@gmail.com>
2019-07-15 00:10:00 +0100

    omg all done with header and slide (^_^)

---
+++ js/myBlugns.js
@@ -0,0 +1,12 @@
+var slide = document.getElementById('slide')
+var humborger = document.getElementById('toggle-btn');
+var navBar = document.querySelector('header')
+
+
+
+
+
+humborger.onclick = function () {
+     navBar.classList.toggle('fade')
+
+}
