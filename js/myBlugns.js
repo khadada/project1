ab6b86d1a977bfcf144c8fd6b977368875d62e60
khadada <lkhadada@gmail.com>
2019-07-21 21:46:34 +0100

    debug my navigation problem

--- js/myBlugns.js
+++ js/myBlugns.js
@@ -1,12 +1,13 @@
 var slide = document.getElementById('slide')
 var humborger = document.getElementById('toggle-btn');
-var navBar = document.querySelector('header')
+var navBar = document.querySelector('.header-row2')
 
 
 
 
 
 humborger.onclick = function () {
-     navBar.classList.toggle('fade')
+     navBar.classList.toggle('slideY');
+     console.log('helllllllllll')
 
 }
